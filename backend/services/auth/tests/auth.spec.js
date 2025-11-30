2ca107ca05cbefb8506ca89eed87f6e4aafe1d5c
Alton <asllanialton@gmail.com>
2025-11-30 21:32:25 +0100

    Add Auth service OpenAPI spec and service entrypoint

---
+++ backend/services/auth/tests/auth.spec.js
@@ -0,0 +1,7 @@
+// backend/services/auth/tests/auth.spec.js
+// Skelet për më vonë – p.sh. me Jest ose Mocha
+describe('Auth service', () => {
+  test('dummy test', () => {
+    expect(true).toBe(true);
+  });
+});
