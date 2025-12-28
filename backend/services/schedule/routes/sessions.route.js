04e70a964d0dd648acb5e7e243684fc346147e1a
Alton <asllanialton@gmail.com>
2025-12-28 22:55:13 +0100

    fix some errors in backend

--- backend/services/schedule/routes/sessions.route.js
+++ backend/services/schedule/routes/sessions.route.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const controller = require('../controllers/sessions.controller');
-const { validate } = require('../../shared/validate');
-const { createSessionSchema, updateSessionSchema } = require('../validators/session.schema');
+const { validate } = require('../../../shared/utils/validate');
+const {
+  createSessionSchema,
+  updateSessionSchema,
+} = require('../validators/session.schema');
 
 const router = express.Router();
 
